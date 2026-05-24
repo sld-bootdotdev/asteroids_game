71f7c79975c0e324142b5e3912152e18b6665980
sld-devops <sandisfifteen@gmail.com>
2026-05-24 11:05:58 +0300

    Implement game state logging and create initial game state data file

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import pygame
 from constants import *
+from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Initialize Pygame
+    pygame.init()
+    # Create game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # Log current game state
+        log_state()
+        # Processing event queue
+        for event in pygame.event.get():
+            # Exit game when window is closed
+            if event.type == pygame.QUIT:
+                return
+        # Fill screen with black
+        screen.fill("black")
+        # Update display
+        pygame.display.flip()
+
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
